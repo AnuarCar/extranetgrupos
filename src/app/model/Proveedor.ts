e59a68f11f25f403c755768d2dee1e0cc4033466
Carabali <uahc020@mycwt.com>
2021-01-21 20:38:03 -0500

    Form con zorro proveedores

---
+++ src/app/model/Proveedor.ts
@@ -0,0 +1,31 @@
+export class Proveedor {
+    idProveedor:Number;
+    tipoProveedor:String;
+    cadena:String;
+    nombre:String;
+    checkin:Date;
+    checout:Date;
+    estrellas:Number;
+    pais:String;
+    ciudad:String;
+    direccion:String;
+    telefono:String;
+    descripcionHotel:String;
+    nombreGerenteGeneral:String;
+    emailGerenteGeneral:String;
+    notificarGerenteGeneral:Boolean;
+    telefonoGerenteGeneral:String;
+    nombreGerenteComercial:String;
+    emailGerenteComercial:String;
+    notificarGerenteComerial:Boolean;
+    telefonoGerenteComercial:String;
+    nombreContactoReservas:String;
+    emailContactoReservas:String;
+    notificarContactoReservas:Boolean;
+    telefonoContactoReservas:String;
+    nombreContactoGrupos:String;
+    emailContactoGrupos:String;
+    notificarContactoGrupos:Boolean;
+    telefonoContactoGrupos:String;
+
+}
