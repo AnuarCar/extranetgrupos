1133d2d599d54e813362f40dab3551dba5cd0320
Anuar Carabali Osuna <anuarcar@gmail.com>
2021-02-06 16:46:00 -0500

    Prov y Contratos Deploy 5

--- src/app/service/proveedor.service.ts
+++ src/app/service/proveedor.service.ts
@@ -3,7 +3,7 @@ import { Proveedor } from '../model/Proveedor';
 import { Tipoproveedor } from '../model/Tipoproveedor';
 import { Observable , ObservableInput } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
   production: true,
-  apiAuthUrl: 'https://inventarioextranet.herokuapp.com'
 };
